b8510f018e0658f2550a1a348382f00ada1b349c
TheMargad <themargadxr@gmail.com>
2023-10-08 18:19:45 +0800

    Chech vercel

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/  z-[2]" />
       <div className="p-5 text-white z-[2] mt-[3rem]">
         <h2 className="text-5xl font-bold text-[#33363B] text-center">
-          Augmented Reality x Science education
+          Augmented Reality x Science Education
         </h2>
         <div className="flex justify-center items-center">
           <p className="py-5 text-xl text-[#33363B]">
